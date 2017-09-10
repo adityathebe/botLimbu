bbdc2fba03bfe724c18c704ca1f6ba88e69936e5
adityathebe <thebeaditya@gmail.com>
2017-09-10 20:22:02 +0545

    Code refactoring

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-    name: { type: String },
+    name: { type: String, required: true },
     fb_id: { type: String, required: true, unique: true },
 });
 
--- utility/subscription.js
+++ utility/subscription.js
@@ -1,17 +1,17 @@
 const UserModel = require('../models/user');
 const BOT = require('../Template/templates');
 const request = require('request');
-const token = process.env.FB_VERIFY_ACCESS_TOKEN;
+const token = process.env.FB_VERIFY_ACCESS_TOKEN || 'EAAB6xJpFXa0BALfsNdvOqV4tBt2poTm61X2eVAZAcENC2q5aTI0TPUgGGbCCDZAxhhxBmnSXCNmeKuLE0nzeVxKe1hbOi8OAU5kud0zOpA46efhkGtFTJJh60mwZBwp7ympZA4tXoCX7PBxwzQQw415rxX2Gv34cUUaoVFFEMqvxjE9fgcwb';
 
 const subscribe = (id) => {
     const url = `https://graph.facebook.com/v2.10/${id}?access_token=${token}`;
     request({url, json: true}, (error, response, body) => {
         var newUser = new UserModel({
-            name: `${body.first_name} ${body.last_name}`,
+            name : `${body.first_name} ${body.last_name}`,
             fb_id: id
         });
 
-        UserModel.findOneAndUpdate({fb_id: newUser.fb_id}, {fb_id: newUser.fb_id}, {upsert:true}, (err, user) => {
+        UserModel.findOneAndUpdate({fb_id: newUser.fb_id}, newUser, {upsert:true}, (err, user) => {
             console.log(`Subscribing : ${id}`);
             if (err) {
                 console.log(err);
