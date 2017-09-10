9f29c5ea3a7314aad4c7775aa6205f725998f15b
adityathebe <thebeaditya@gmail.com>
2017-09-10 20:13:38 +0545

    Code refactoring

--- utility/subscription.js
+++ utility/subscription.js
@@ -1,20 +1,26 @@
 const UserModel = require('../models/user');
 const BOT = require('../Template/templates');
+const request = require('request');
+const token = process.env.FB_VERIFY_ACCESS_TOKEN;
 
 const subscribe = (id) => {
-    var newUser = new UserModel({
-        fb_id: id,
-    });
+    const url = `https://graph.facebook.com/v2.10/${id}?access_token=${token}`;
+    request({url, json: true}, (error, response, body) => {
+        var newUser = new UserModel({
+            name: `${body.first_name} ${body.last_name}`
+            fb_id: id,
+        });
 
-    UserModel.findOneAndUpdate({fb_id: newUser.fb_id}, {fb_id: newUser.fb_id}, {upsert:true}, (err, user) => {
-        console.log(`Subscribing : ${id}`);
-        if (err) {
-            console.log(err);
-            BOT.sendTextMessage(id, "There was an error subscribing.");
-        } else {
-            console.log('User saved successfully!');
-            BOT.sendTextMessage(newUser.fb_id, "You've been subscribed!")
-        }
+        UserModel.findOneAndUpdate({fb_id: newUser.fb_id}, {fb_id: newUser.fb_id}, {upsert:true}, (err, user) => {
+            console.log(`Subscribing : ${id}`);
+            if (err) {
+                console.log(err);
+                BOT.sendTextMessage(id, "There was an error subscribing.");
+            } else {
+                console.log('User saved successfully!');
+                BOT.sendTextMessage(newUser.fb_id, "You've been subscribed!")
+            }
+        });
     });
 }
 
