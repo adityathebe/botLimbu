6b3ecd44a8c43dd17fe4447a9107a4348b7810a1
adityathebe <thebeaditya@gmail.com>
2017-09-10 19:42:20 +0545

    Code refactoring

--- models/user.js
+++ models/user.js
@@ -1,29 +1,8 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-// create a schema
-var userSchema = new Schema({
-  name: String,
-  fb_id: { type: String, required: true, unique: true },
-  created_at: Date,
-  updated_at: Date
+var userSchema = mongoose.Schema({
+    name: { type: String },
+    fb_id: { type: String, required: true, unique: true },
 });
 
-var User = mongoose.model('User', userSchema);
-
-userSchema.pre('update', function(next) {
-  // get the current date
-  var currentDate = new Date();
-  
-  // change the updated_at field to current date
-  this.updated_at = currentDate;
-
-  // if created_at doesn't exist, add to that field
-  if (!this.created_at)
-    this.created_at = currentDate;
-
-  next();
-});
-
-// make this available to our users in our Node applications
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);;
--- utility/subscription.js
+++ utility/subscription.js
@@ -1,7 +1,5 @@
-const mongoose = require('mongoose');
-
 const UserModel = require('../models/user');
-const BOT = require('../Template/templates.js');
+const BOT = require('../Template/templates');
 
 const subscribe = (id) => {
     var newUser = new UserModel({
@@ -10,7 +8,7 @@ const subscribe = (id) => {
 
     UserModel.findOneAndUpdate({fb_id: newUser.fb_id}, {fb_id: newUser.fb_id}, {upsert:true}, (err, user) => {
         if (err) {
-            BOT.sendTextMessage(id, "There wan error subscribing you for daily articles");
+            BOT.sendTextMessage(id, "There was an error subscribing.");
         } else {
             console.log('User saved successfully!');
             BOT.sendTextMessage(newUser.fb_id, "You've been subscribed!")
@@ -19,10 +17,9 @@ const subscribe = (id) => {
 }
 
 const unsubscribe = (id) => {
-  // call the built-in save method to save to the database
-    UserModel.findOneAndRemove({fb_id: id}, function(err, user) {
+    UserModel.findOneAndRemove({fb_id: id}, (err, user) => {
         if (err) {
-            BOT.sendTextMessage(id, "There wan error unsubscribing you for daily articles");
+            BOT.sendTextMessage(id, "There was an error unsubscribing.");
         } else {
             console.log('User deleted successfully!');
             BOT.sendTextMessage(id, "You've been unsubscribed!")
