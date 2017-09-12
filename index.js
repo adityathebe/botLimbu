aa29e06085a755d3151fa370390517460662b2b8
adityathebe <thebeaditya@gmail.com>
2017-09-12 17:56:31 +0545

    'Subscription'

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-"use strict";
-require('dotenv').config();
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
