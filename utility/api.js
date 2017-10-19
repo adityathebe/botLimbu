e76f65af1b5e7a365fcb06d3452c514b3c88f8dd
adityathebe <thebeaditya@gmail.com>
2017-10-19 06:52:03 +0545

    Error handling API.AI

--- utility/api.js
+++ utility/api.js
@@ -1,18 +1,25 @@
 const apiai = require('apiai');
-const app = apiai(process.env.APIAI);
+// const app = apiai(process.env.APIAI);
+const app = apiai('5224b6598e8b45858b9babb233c4f0f6');
 
 const callAPI = (query) => {
     return new Promise((resolve, reject) => {
         app.textRequest(query, {
             sessionId: '123456789'
         }).on('response', (response) => {
-            resolve({
-                speech : response.result.fulfillment.messages[0].speech,
-                action : response.result.action,
-                intent : response.result.metadata.intentName,
-                incomplete : response.result.actionIncomplete,
-                parameters : response.result.parameters,
-            });
+            let reply;
+            try {                    
+                reply = {
+                    speech : response.result.fulfillment.messages[0].speech,
+                    action : response.result.action,
+                    intent : response.result.metadata.intentName,
+                    incomplete : response.result.actionIncomplete,
+                    parameters : response.result.parameters,
+                };
+            } catch(err) {
+                reject(err);
+            }
+            resolve(reply)
         }).on('error', (error) => {
             reject(error);
         }).end();      
